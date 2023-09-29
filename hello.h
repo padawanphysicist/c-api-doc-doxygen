11d2d2442fccafd397e8d413fc471f47c60fb820
Victor Santos (vct) <victor_santos@fisica.ufc.br>
2023-09-28 23:24:20 -0300

    Initial commit

---
+++ hello.h
@@ -0,0 +1,19 @@
+/// @file hello.h
+#ifndef __HELLO_H__
+#define __HELLO_H__
+
+/**
+ * The answer to the ultimate question about life, the Universe, and
+ * everything.
+ */
+#ifndef MY_CONST
+#    define MY_CONST 42
+#endif
+
+/**
+ * Say hello!
+ */
+void
+hello (void);
+
+#endif /* __HELLO_H__ */
